a63f2ab9efae77c354e4288cba4efe0eb7d8549d
Mr Speaker <mrspeaker@gmail.com>
2011-05-17 12:06:49 +0200

    remove spaces

--- src/howami.c
+++ src/howami.c
@@ -1,7 +1,7 @@
-#include <stdio.h>                                                             
-                                                                                
-int main(int argc, char **argv)                                                 
-{                                                                               
-    printf("Fine.\n");                                                          
-    return 0;                                                                   
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("Fine.\n");
+    return 0;
 }
